224c8bc1fb0b8fd447775fefe6a2052a50fa7331
User <User@LAB2-7>
2024-03-26 14:42:26 +0200

    kt husg, nv gauhjlk

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,11 +1,41 @@
-﻿namespace TriviaAppClean.ViewModels
+﻿using TriviaAppClean.Models;
+namespace TriviaAppClean.ViewModels
 {
     public class ShellViewModel : ViewModelBase
     {
-        //private bool IsAdmin;
-        //public bool IsAdmin
-        //{
-            
-        //}
+        public ShellViewModel()
+        {
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute("connectingToServer", typeof(ConnectingToServerView));
+        }
+        public bool IsAdmin
+        {
+            get
+            {
+                User currentUser = ((App)Application.Current).LoggedInUser;
+                if (currentUser == null || currentUser.Rank < 2)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public bool IsMaster
+        {
+            get
+            {
+                User currentUser = ((App)Application.Current).LoggedInUser;
+                if (currentUser != null || currentUser.Rank ==1)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
     }
 }
